7f1dd69eb70670d75610f25d3ccafb6442b84706
suuuchan <aynsuu25@gmail.com>
2021-11-13 22:55:17 +0900

    複数関数

--- sample.js
+++ sample.js
@@ -1,9 +1,16 @@
-var str = "webcamp"
+let user_hand = prompt("ジャンケンの手をグー、チョキ、パーから選んでください。");
+alert("あなたの選んだ手は" + user_hand + "です。")
 
-function foo() {
-  console.log(str)
-  var str = "dmm webcamp"
-  console.log(str)
+function getJShand(){
+  let js_hand_num = Math.floor(Math.random()*3);
+
+  if(js_hand_num == 0){
+    js_hand = "グー";
+  }else if(js_hand_num == 1){
+    js_hand = "チョキ";
+  }else if(js_hand_num == 2){
+    js_hand = "パー";
+  }
+
+  return js_hand;
 }
-
-foo()
