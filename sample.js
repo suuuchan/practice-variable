3c1e013d337c33a220b219e8c064e9d9a9ccb74f
suuuchan <aynsuu25@gmail.com>
2021-11-28 23:15:01 +0900

    constを使う意味

--- sample.js
+++ sample.js
@@ -1,10 +1,23 @@
-let max = 190;
-let num = 1;
-let count = 0;
+var kinu1 = 1;
+var kinu1 = 2;
 
-while(num < max){
-  num = num * 2;
-  count = count + 1;
+let kinu1 = 1;
+let kinu1 = 2; //error
+
+function ayane1 (){
+  var mugi1 = 1;
+  var mugi2 = 2;
+  let kinu3;
+  const mugi5 = 3;
+  const mugi5 = 4;
+  return mugi1+mugi2+mugi5;
 }
 
-alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+function ayane2(){
+  alert(mugi5);
+}
+alert(kinu3);
+const mugi6 = 6;
+alert(mugi6);
+var mugi3 = ayane1()
+var mugi4 = mugi1+mugi2+mugi5;
