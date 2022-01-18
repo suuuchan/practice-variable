6beb710b6a29dd3d69093ef2cd65f7e35dcca74c
suuuchan <aynsuu25@gmail.com>
2022-01-18 23:30:40 +0900

    あみだくじ試行中

--- sample.js
+++ sample.js
@@ -1,56 +1,93 @@
-let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
-while ((user_hand != "グー") && (user_hand != "チョキ") && (user_hand != "パー") && (user_hand != null)){
-  alert('グー・チョキ・パーのいずれかを入力して下さい');
-  user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
-}
-let js_hand = getJShand();
-let judge = winLose(user_hand, js_hand);
+{
+  'use strict'
+  function makeAmida() {
+      const canvas = document.getElementById('canvas');
+      if (!canvas.getContext || !canvas){
+          return;
+      }
+      const ctx = canvas.getContext('2d');
+      const tree = 5;
+      const treeHeight = 15;
 
-if (user_hand != null){
-  alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
-} else {
-  alert("またチャレンジしてね")
-}
+      function random() {
+          return Math.floor(Math.random() * 2);
+      }
 
-function getJShand(){
-  let js_hand_num = Math.floor(Math.random() * 3);
-  let hand;
-  if (js_hand_num == 0){
-    hand = "グー";
-  } else if (js_hand_num == 1){
-    hand = "チョキ";
-  } else if (js_hand_num == 2){
-    hand = "パー";
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.strokeStyle = 'black';
+
+      const redLine = [];
+      let redPoint;
+
+      for (let y = 1; y <= treeHeight; y++){
+          redPoint = 0;
+          redLine[y - 1] = [redPoint];
+
+          for (let x = 1; x <= tree; x++){
+              if (x === tree || random() === 0 || y === treeHeight){
+                  redLine[y - 1][redPoint] = [0, 0];
+                  redPoint++;
+
+                  ctx.beginPath();
+                  ctx.moveTo( 50 * x ,y * 30 );
+                  ctx.lineTo( 50 * x ,((y + 1) * 30));
+                  ctx.stroke();
+              } else {
+                  for (let t = 0; t < 2; t++){  
+                      redLine[y - 1][redPoint]= [x, x + 1];
+                      redPoint++;
+                  }
+                  ctx.beginPath();
+                  ctx.moveTo( 50 * x, y * 30 );
+                  ctx.lineTo( 50 * x,((y + 1) * 30));
+                  ctx.lineTo( 50 * (x + 1),((y + 1) * 30));
+                  ctx.moveTo( 50 * (x + 1), y * 30 );
+                  ctx.lineTo( 50 * (x + 1),((y + 1) * 30));
+                  ctx.stroke();
+                  x++;
+              }
+          }
+      }
+      const undo = ctx.getImageData(0, 0, canvas.width, canvas.height); 
+
+      ctx.strokeStyle = '#e65353';
+      ctx.lineWidth = 3;
+
+      for (let x = 1; x <= tree; x++){
+          input = document.createElement('input');
+          input.type = "submit";
+          input.value = "たどる"
+
+          input.addEventListener('click', () =>{
+              ctx.putImageData(undo, 0, 0);                   
+              r = x;                                                              
+
+              for(let y = 1; y <= treeHeight; y++){
+                      ctx.moveTo( 50 * x, y * 30 );
+                  if(redLine[y - 1][x - 1][0] === x){ 
+                      ctx.moveTo( 50 * x, y * 30 );
+                      ctx.lineTo( 50 * x,((y + 1) * 30));
+                      ctx.lineTo( 50 * (x + 1),((y + 1) * 30));
+                      ctx.stroke();
+                      x++;
+                  } else if(redLine[y - 1][x - 1][1] === x){
+                      ctx.beginPath();
+                      ctx.moveTo( 50 * x, y * 30 );
+                      ctx.lineTo( 50 * x,((y + 1) * 30));
+                      ctx.lineTo( 50 * (x - 1),((y + 1) * 30));
+                      ctx.stroke();
+                      x--;
+                  } else{
+                      ctx.beginPath();
+                      ctx.moveTo( 50 * x ,y * 30 );
+                      ctx.lineTo( 50 * x ,((y + 1) * 30));
+                      ctx.stroke();
+                  }
+              }
+              x = r; 
+          });
+          document.body.appendChild(input);
+      }
   }
-  return hand;
-}
-
-function winLose(user, js){
-  let winLoseStr;
-  if (user == "グー"){
-    if (js == "グー"){
-      winLoseStr = "あいこ";
-    } else if (js == "チョキ"){
-      winLoseStr = "勝ち";
-    } else if (js == "パー"){
-      winLoseStr = "負け";
-    }
-  } else if (user == "チョキ"){
-    if (js == "グー"){
-      winLoseStr = "負け";
-    } else if (js == "チョキ"){
-      winLoseStr = "あいこ";
-    } else if (js == "パー"){
-      winLoseStr = "勝ち";
-    }
-  } else if (user == "パー"){
-    if (js == "グー"){
-      winLoseStr = "勝ち";
-    } else if (js == "チョキ"){
-      winLoseStr = "負け";
-    } else if (js == "パー"){
-      winLoseStr = "あいこ";
-    }
-  }
-  return winLoseStr;
+  makeAmida();
 }
