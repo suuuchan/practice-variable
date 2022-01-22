abbc94229800c18af477ead2151772489f0df1f1
suuuchan <aynsuu25@gmail.com>
2022-01-22 23:51:52 +0900

    時計途中編

--- sample.js
+++ sample.js
@@ -1,93 +1,40 @@
 {
-  'use strict'
-  function makeAmida() {
-      const canvas = document.getElementById('canvas');
-      if (!canvas.getContext || !canvas){
-          return;
-      }
-      const ctx = canvas.getContext('2d');
-      const tree = 4;
-      const treeHeight = 12;
+  const clock = () => {
 
-      function random() {
-          return Math.floor(Math.random() * 2);
-      }
+    const d = new Date();
+  
 
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.strokeStyle = 'black';
+    let year = d.getFullYear();
 
-      const redLine = [];
-      let redPoint;
+    let month = d.getMonth() + 1;
 
-      for (let y = 1; y <= treeHeight; y++){
-          redPoint = 0;
-          redLine[y - 1] = [redPoint];
+    let date = d.getDate();
+ 
+    let dayNum = d.getDay();
+    const weekday = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+    let day = weekday[dayNum];
+ 
+    let hour = d.getHours();
 
-          for (let x = 1; x <= tree; x++){
-              if (x === tree || random() === 0 || y === treeHeight){
-                  redLine[y - 1][redPoint] = [0, 0];
-                  redPoint++;
-
-                  ctx.beginPath();
-                  ctx.moveTo( 50 * x ,y * 30 );
-                  ctx.lineTo( 50 * x ,((y + 1) * 30));
-                  ctx.stroke();
-              } else {
-                  for (let t = 0; t < 2; t++){  
-                      redLine[y - 1][redPoint]= [x, x + 1];
-                      redPoint++;
-                  }
-                  ctx.beginPath();
-                  ctx.moveTo( 50 * x, y * 30 );
-                  ctx.lineTo( 50 * x,((y + 1) * 30));
-                  ctx.lineTo( 50 * (x + 1),((y + 1) * 30));
-                  ctx.moveTo( 50 * (x + 1), y * 30 );
-                  ctx.lineTo( 50 * (x + 1),((y + 1) * 30));
-                  ctx.stroke();
-                  x++;
-              }
-          }
-      }
-      const undo = ctx.getImageData(0, 0, canvas.width, canvas.height); 
-
-      ctx.strokeStyle = '#e65353';
-      ctx.lineWidth = 3;
-
-      for (let x = 1; x <= tree; x++){
-          input = document.createElement('input');
-          input.type = "submit";
-          input.value = "たどる"
-
-          input.addEventListener('click', () =>{
-              ctx.putImageData(undo, 0, 0);                   
-              r = x;                                                              
-
-              for(let y = 1; y <= treeHeight; y++){
-                      ctx.moveTo( 50 * x, y * 30 );
-                  if(redLine[y - 1][x - 1][0] === x){ 
-                      ctx.moveTo( 50 * x, y * 30 );
-                      ctx.lineTo( 50 * x,((y + 1) * 30));
-                      ctx.lineTo( 50 * (x + 1),((y + 1) * 30));
-                      ctx.stroke();
-                      x++;
-                  } else if(redLine[y - 1][x - 1][1] === x){
-                      ctx.beginPath();
-                      ctx.moveTo( 50 * x, y * 30 );
-                      ctx.lineTo( 50 * x,((y + 1) * 30));
-                      ctx.lineTo( 50 * (x - 1),((y + 1) * 30));
-                      ctx.stroke();
-                      x--;
-                  } else{
-                      ctx.beginPath();
-                      ctx.moveTo( 50 * x ,y * 30 );
-                      ctx.lineTo( 50 * x ,((y + 1) * 30));
-                      ctx.stroke();
-                  }
-              }
-              x = r; 
-          });
-          document.body.appendChild(input);
-      }
-  }
-  makeAmida();
-}
+    let min = d.getMinutes();
+  
+    let sec = d.getSeconds();
+  
+   
+    month = month < 10 ? "0" + month : month;
+    date = date < 10 ? "0" + date : date;
+    hour = hour < 10 ? "0" + hour : hour;
+    min = min < 10 ? "0" + min : min;
+    sec = sec < 10 ? "0" + sec : sec;
+  
+   
+    let today = `${year}.${month}.${date} ${day}`;
+    let time = `${hour}:${min}:${sec}`;
+  
+ 
+    document.querySelector(".clock-date").innerText = today;
+    document.querySelector(".clock-time").innerText = time;
+  };
+  
+  
+  setInterval(clock, 1000);
